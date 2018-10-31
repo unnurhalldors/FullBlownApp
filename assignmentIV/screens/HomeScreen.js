ff60b7ccdadc8d6819b07234ea9c7891f63c607f
Hreidar Kristinn <hreidarkristinn@clients-pool-1.nat.ru.is>
2018-10-31 14:36:21 +0000

    fix

--- assignmentIV/screens/HomeScreen.js
+++ assignmentIV/screens/HomeScreen.js
@@ -41,9 +41,9 @@ export default class HomeScreen extends React.Component {
     return (
       <View style={styles.concertContainer}>
         <Image style={styles.image} source={{ uri: item.imageSource }} />
-        <View styles={styles.concertInfo}>
+        <View style={styles.concertInfo}>
           <Text>{item.name}</Text>
-          <Text>{item.dateOfShow}</Text>
+          <Text>{new Date(item.dateOfShow).toLocaleString("is-IS")}</Text>
           <Text>{item.eventDateName}</Text>
           <Text>{item.eventHallName}</Text>
           <Text>{item.userGroupName}</Text>
@@ -78,94 +78,12 @@ const styles = StyleSheet.create({
     paddingBottom: 16
   },
   concertInfo: {
-    paddingLeft: 20,
+    paddingLeft: 8,
     justifyContent: "space-around",
     flex: 1
   },
   image: {
     height: 125,
     width: 125
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
-  },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
   }
 });
