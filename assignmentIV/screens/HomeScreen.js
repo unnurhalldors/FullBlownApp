b4b22be54d9ef09f3b31bf5ea6d87f7cb6567834
Hreidar Kristinn <hreidarkristinn@clients-pool-1.nat.ru.is>
2018-11-01 14:56:08 +0000

    search virkni

--- assignmentIV/screens/HomeScreen.js
+++ assignmentIV/screens/HomeScreen.js
@@ -119,10 +119,30 @@ class HomeScreen extends React.Component {
     this.setState({ searchString: data });
   };
 
+  filteredData = (concerts) => {
+    const results = [];
+    const { searchString } = this.state;
+
+    if (searchString !== '') {
+      for (let i = 0; i < concerts.length; i++) {
+        for (key in concerts[i]) {
+          if (typeof concerts[i][key] === 'string') {
+            if (concerts[i][key].indexOf(searchString) != -1) {
+              if (!results.includes(concerts[i])) {
+                results.push(concerts[i]);
+              }
+            }
+          }
+        }
+      }
+      return results;
+    }
+    return concerts;
+  };
+
   render() {
     const { searchString } = this.state;
     const { concerts } = this.props;
-    console.log(concerts);
 
     return (
       <View style={styles.container}>
@@ -139,7 +159,7 @@ class HomeScreen extends React.Component {
         <ScrollView contentContainerStyle={styles.listContainer}>
           <FlatList
             ListEmptyComponent={<ActivityIndicator size="large" />}
-            data={concerts}
+            data={this.filteredData(concerts)}
             renderItem={this.renderItem}
             keyExtractor={(item, index) => index.toString()}
             ListFooterComponent={<ListFooter />}
