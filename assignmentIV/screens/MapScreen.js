8eff079386bfce1ea76f272ecf5843932ab8bce2
evasif <evasiif@gmail.com>
2018-11-01 14:16:13 +0000

    MAP DISPLAYED

--- assignmentIV/screens/MapScreen.js
+++ assignmentIV/screens/MapScreen.js
@@ -1,27 +1,49 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-
-export default class MapScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Map',
-    };
-
-    render() {
-        return (
-            <ScrollView style={styles.container}>
-                {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-                <ExpoLinksView />
-            </ScrollView>
-        );
-    }
-}
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 15,
-        backgroundColor: '#fff',
-    },
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
+
+export default class MapScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Map',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          showsUserLocation
+          showsMyLocationButton
+          followsUserLocation
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 64.12375,
+            longitude: -21.92563,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    );
+  }
+}
