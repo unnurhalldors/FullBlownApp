4fc30fb591d51feda264dc491b77d0822d3df4f9
Unnur Lára Halldórsdóttir <unnurh16@ru.is>
2018-11-07 14:40:11 +0000

    minor fix

--- assignmentIV/components/ListHeader.js
+++ assignmentIV/components/ListHeader.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   View, Text, StyleSheet, TextInput,
 } from 'react-native';
-import { Icon } from 'expo';
 
 /* All styles */
 const styles = StyleSheet.create({
@@ -14,7 +13,6 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
   },
   searchContainer: {
-    flexDirection: 'row',
     borderBottomWidth: 0.5,
     borderBottomColor: '#ccc',
     width: '100%',
@@ -22,15 +20,11 @@ const styles = StyleSheet.create({
     paddingTop: 10,
   },
   search: {
-    flex: 14,
     paddingLeft: 10,
     fontSize: 18,
     backgroundColor: '#f2f2f2',
     height: 40,
     borderRadius: 10,
-  },
-  iconStyle: {
-    flex: 1,
   },
 });
 
@@ -42,7 +36,6 @@ class Header extends React.PureComponent {
       <View>
         <Text style={styles.headerText}>Concerts</Text>
         <View style={styles.searchContainer}>
-          <Icon.FontAwesome style={styles.iconStyle} name="search" size={18} color="#a8a6a6" />
           <TextInput
             placeholder="Search"
             placeholderTextColor="#a8a6a6"
